133eab6fb68eae169dc0424c2da86a82616c2fd1
darshnablah <dwright758@gmail.com>
2020-06-05 17:42:47 -0500

    added number selections to shop

--- assets/js/game.js
+++ assets/js/game.js
@@ -106,10 +106,7 @@ var startGame = function() {
             shop();
             }
         }
-        // else {
-        //     window.alert("You have lost your robot in battle! Game Over!");
-        //     break;
-        // }
+        
     }
 
     //after the loop ends, player is either out of health or opponents, so run endgame function
@@ -139,23 +136,20 @@ var endGame = function() {
 
 var shop = function() {
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
-
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     //use switch to carry out action
     switch (shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
 
-        case "UPGRADE":    
-        case "upgrade":
+        case 2:    
             playerInfo.upgradeAttack()
             break;
 
-        case "LEAVE":    
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
             //do nothing so function will end
             break;
